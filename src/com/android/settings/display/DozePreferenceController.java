643591dce2ae6493c1819d4fc12c748b73eebdc3
Adrian DC <radian.dc@gmail.com>
2017-10-03 12:22:32 +0200

    Revert "Display: Allow hidding the Doze Ambient Display preference"
    This reverts commit a5af20fb9209f756be3812bd633db0a526b44767.

--- src/com/android/settings/display/DozePreferenceController.java
+++ src/com/android/settings/display/DozePreferenceController.java
@@ -69,10 +69,6 @@ public class DozePreferenceController extends PreferenceController implements
     @Override
     public boolean isAvailable() {
         String name = Build.IS_DEBUGGABLE ? SystemProperties.get("debug.doze.component") : null;
-        if (TextUtils.isEmpty(name) &&
-                SystemProperties.get("ro.settings.doze.hide.preference").equals("true")) {
-            return false;
-        }
         if (TextUtils.isEmpty(name)) {
             name = mContext.getResources().getString(
                     com.android.internal.R.string.config_dozeComponent);
